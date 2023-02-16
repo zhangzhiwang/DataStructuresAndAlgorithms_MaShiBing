71b538c7aa432851676a8a65f18743a21049ef9f
zhangzhiwang <wb_zhangzhiwang@kuaishou.com>
2023-02-16 14:18:05 +0800

    日常提交

--- src/main/java/junior/RandomA2B.java
+++ src/main/java/junior/RandomA2B.java
@@ -13,84 +13,84 @@ package junior;
  * 所以6可以用三个二进制位存放，每个二进制位都有0和1两种值，所以每个二进制位调用一下发生器即可，然后独立调用三次发生器，分别将结果左移2位、1位和0位，将左移后的结果加起来就得到了0-7范围的数。
  * 但我们的前期目标是0-6，那么如果生成7的话就重做，直到生成不是7为止，这样把生成7的概率平摊到0-6当中，所以重做后生成的0-6的概率依然是相等的。
  * （2）将上一部生成的0-6的函数的返回值+1就是1-7范围内的随机整数了且等概率。
- * 
+ *
  * @author zhangzhiwang
  * @date 2022年1月18日 下午9:02:09
  */
 public class RandomA2B {
-	public static void main(String[] args) {
-		int[] arr = new int[8];
-		for(int i = 0; i < 10000; i++) {
-			arr[c()]++;
-		}
-		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(String.format("生成整数%d的次数是%d", i, arr[i]));
-		}
-	}
+    public static void main(String[] args) {
+        int[] arr = new int[8];
+        for (int i = 0; i < 10000; i++) {
+            arr[c()]++;
+        }
 
-	/**
-	 * 题目已知函数，只能使用不能修改
-	 * 
-	 * @return
-	 * @author zhangzhiwang
-	 * @date 2022年1月18日 下午9:24:02
-	 */
-	public static int f() {
-		return (int) (Math.random() * 5) + 1;// 这里取得是整数范围在1-5，由于double强转int是向下取整，所以Math.random()要乘以5而不是4。
-	}
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(String.format("生成整数%d的次数是%d", i, arr[i]));
+        }
+    }
 
-	/**
-	 * 0、1发生器
-	 * 
-	 * @return
-	 * @author zhangzhiwang
-	 * @date 2022年1月18日 下午9:28:39
-	 */
-	public static int g() {
-		int result = 0;
-		do {
-			result = f();
-		} while (result == 3);// f函数返回3重做
+    /**
+     * 题目已知函数，只能使用不能修改
+     *
+     * @return
+     * @author zhangzhiwang
+     * @date 2022年1月18日 下午9:24:02
+     */
+    public static int f() {
+        return (int) (Math.random() * 5) + 1;// 这里取得是整数范围在1-5，由于double强转int是向下取整，所以Math.random()要乘以5而不是4。
+    }
 
-		return result == 1 || result == 2 ? 0 : 1;
-	}
+    /**
+     * 0、1发生器
+     *
+     * @return
+     * @author zhangzhiwang
+     * @date 2022年1月18日 下午9:28:39
+     */
+    public static int g() {
+        int result = 0;
+        do {
+            result = f();
+        } while (result == 3);// f函数返回3重做
 
-	/**
-	 * 将输出范围调整到0-7
-	 * 
-	 * @return
-	 * @author zhangzhiwang
-	 * @date 2022年1月18日 下午9:31:40
-	 */
-	public static int a() {
-		return (g() << 2) + (g() << 1) + (g() << 0);// 注意加号的优先级要高于位运算，所以位运算必须加括号
-	}
+        return result == 1 || result == 2 ? 0 : 1;
+    }
 
-	/**
-	 * 利用a()函数将0-7范围调整到0-6
-	 * 
-	 * @return
-	 * @author zhangzhiwang
-	 * @date 2022年1月18日 下午9:38:33
-	 */
-	public static int b() {
-		int result = 0;
-		do {
-			result = a();
-		} while (result > 6);
-		
-		return result;
-	}
-	
-	/**
-	 * 输出题目要求的结果1-7
-	 * 
-	 * @return
-	 * @author zhangzhiwang
-	 * @date 2022年1月18日 下午9:40:05
-	 */
-	public static int c() {
-		return b() + 1;
-	}
+    /**
+     * 将输出范围调整到0-7
+     *
+     * @return
+     * @author zhangzhiwang
+     * @date 2022年1月18日 下午9:31:40
+     */
+    public static int a() {
+        return (g() << 2) + (g() << 1) + (g() << 0);// 注意算数运算符的优先级要高于位运算，所以位运算必须加括号
+    }
+
+    /**
+     * 利用a()函数将0-7范围调整到0-6
+     *
+     * @return
+     * @author zhangzhiwang
+     * @date 2022年1月18日 下午9:38:33
+     */
+    public static int b() {
+        int result = 0;
+        do {
+            result = a();
+        } while (result > 6);
+
+        return result;
+    }
+
+    /**
+     * 输出题目要求的结果1-7
+     *
+     * @return
+     * @author zhangzhiwang
+     * @date 2022年1月18日 下午9:40:05
+     */
+    public static int c() {
+        return b() + 1;
+    }
 }
--- src/main/java/junior/dailyPractice/_20221220/PracticeRandomA2B.java
+++ src/main/java/junior/dailyPractice/_20221220/PracticeRandomA2B.java
@@ -9,30 +9,91 @@ import junior.sort.SortCommonUtil;
 public class PracticeRandomA2B {
     /**
      * 已知函数f：等概率发生器
+     *
      * @return
      */
     private static int f() {
-        return (int)(Math.random() * 5) + 1;
+        return (int) (Math.random() * 5) + 1;
     }
+
     /**
      * 0、1等概率发生器
+     *
      * @return
      */
     private static int f1() {
         int i = 0;
         do {
             i = f();
-        } while(i == 3);
+        } while (i == 3);
 
         return i < 3 ? 0 : 1;
     }
 
+    /**
+     * [0,6]范围的最大值6需要3个二进制位才能满足，所以调用等概率发生函数f1三次，分别移动到相关的位置上，然后加起来。
+     * 但是三个二进制位的最大值是7，所以本函数f2的结果是[0,7]等概率返回一个
+     * @return
+     */
+    private static int f2() {
+//        int i1 = f1() << 2;
+//        System.out.println("i1 = " + i1);
+//        int i2 = f1() << 1;
+//        System.out.println("i2 = " + i2);
+//        int i3 = f1();
+//        System.out.println("i3 = " + i3);
+//        return i1 + i2 + i3;
+
+        return (f1() << 2) + (f1() << 1) + f1();// 注意：算术运算符的优先级比位运算符高，所以要加括号
+//        return (int)(f1() << 2) + (int)(f1() << 1) + (int)f1();
+    }
+
+    /**
+     * 将[0,7]范围修正为[0,6]范围
+     * @return
+     */
+    private static int f3() {
+        int i = 0;
+        do {
+            i = f2();
+        } while(i == 7);
+
+        return i;
+    }
+
+    /**
+     * 目标函数g——等概率返回[1,7]范围内的数
+     * @return
+     */
+    private static int g() {
+        return f3() + 1;
+    }
+
+
     public static void main(String[] args) {
-//        int testTimes = 100_0000;
-//        int[] arr = new int[6];
+        int testTimes = 100_0000;
+        int count = 0;
+        int[] arr = new int[6];
 //        for (int i = 0; i < testTimes; i++) {
 //            arr[f()]++;
 //        }
 //        SortCommonUtil.printArr(arr);
+
+//        for (int i = 0; i < testTimes; i++) {
+//            if (f1() == 0) {
+//                count++;
+//            }
+//        }
+//
+//        System.out.println((double) count / (double) testTimes);
+
+//        System.out.println(f2());
+
+        arr = new int[8];
+        for (int i = 0; i < testTimes; i++) {
+            arr[g()]++;
+        }
+
+        SortCommonUtil.printArr(arr);
     }
 }
